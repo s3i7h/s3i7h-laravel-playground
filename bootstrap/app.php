178ed94e6c0b446fc71025cfd495cb3c2b802394
Yuichiro Smith <yu-ichiro@s3i7h.com>
2025-01-08 16:47:42 +0900

    chore: set / as health path

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,7 +8,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        health: '/',
         apiPrefix: '',
     )
     ->withMiddleware(function (Middleware $middleware) {
